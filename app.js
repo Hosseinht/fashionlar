4d861dd33cde864477aea0e7c5936bc04897de17
Ryan Taheri <sailantht@gmail.com>
2021-04-18 18:17:21 +0430

    4.GSAP-2

--- app.js
+++ app.js
@@ -13,8 +13,13 @@ function animateSlides() {
     const img = slide.querySelector("img");
     const revealText = slide.querySelector(".reveal-text");
     //GSAP.
-    gsap.to(revealImg, 1, { x: "100%" });
-    gsap.to(img, 1, { scale: 2 });
+    const slideTl = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut" },
+    });
+    slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
+    slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
+    slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.7");
+    slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.8");
   });
 }
 animateSlides();
